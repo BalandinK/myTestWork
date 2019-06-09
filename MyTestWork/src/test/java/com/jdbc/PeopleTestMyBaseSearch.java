44b7a8b62840c96145fbdc67d1a7299cbf1ac9c1
Kirill Balandin <kbalandin818@gmail.com>
2019-06-09 16:53:49 +0300

    First realisation

---
+++ MyTestWork/src/test/java/com/jdbc/PeopleTestMyBaseSearch.java
@@ -0,0 +1,15 @@
+package com.jdbc;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class PeopleTestMyBaseSearch {
+
+    @Test
+    public void myBaseSearch() {
+
+    }
+}
---
+++ MyTestWork/src/test/java/com/jdbc/PeopleTestMyBaseUpdate.java
@@ -0,0 +1,17 @@
+package com.jdbc;
+
+import org.junit.Test;
+
+import java.sql.Connection;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class PeopleTestMyBaseUpdate {
+
+    @Test
+    public void myBaseUpdate() {
+
+    }
+}
